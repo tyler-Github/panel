e804eda70e4411d31af2eae6846bae84d45ef113
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2021-08-16 19:55:06 +0100

    Add files via upload

---
+++ UGCCPanel/js/redips-scroll.js
@@ -0,0 +1,62 @@
+﻿// define functions
+var setScroll,
+    myScroll;
+
+
+// if query string in URL contains scroll=nnn, then scroll position will be restored 
+setScroll = function () {
+    // get query string parameter with "?"
+    var search = window.location.search,
+        matches;
+    // if query string exists
+    if (search) {
+        // find scroll parameter in query string
+        matches = /scroll=(\d+)/.exec(search);
+        // jump to the scroll position if scroll parameter exists
+        if (matches) {
+            window.scrollTo(0, matches[1]);
+        }
+    }
+};
+
+
+// function appends scroll parameter to the URL or returns scroll value
+myScroll = function (url) {
+    var scroll, q;
+    // Netscape compliant
+    if (typeof (window.pageYOffset) === 'number') {
+        scroll = window.pageYOffset;
+    }
+        // DOM compliant
+    else if (document.body && document.body.scrollTop) {
+        scroll = document.body.scrollTop;
+    }
+        // IE6 standards compliant mode
+    else if (document.documentElement && document.documentElement.scrollTop) {
+        scroll = document.documentElement.scrollTop;
+    }
+        // needed for IE6 (when vertical scroll bar is on the top)
+    else {
+        scroll = 0;
+    }
+    // if input parameter does not exist then return scroll value
+    if (url === undefined) {
+        return scroll;
+    }
+        // else append scroll parameter to the URL
+    else {
+        // set "?" or "&" before scroll parameter
+        q = url.indexOf('?') === -1 ? '?' : '&';
+        // refresh page with scroll position parameter
+        window.location.href = url + q + 'scroll=' + scroll;
+    }
+};
+
+
+// add onload event listener
+if (window.addEventListener) {
+    window.addEventListener('load', setScroll, false);
+}
+else if (window.attachEvent) {
+    window.attachEvent('onload', setScroll);
+}
---
+++ UGCCPanel/js/ugcc.js
@@ -0,0 +1,49 @@
+﻿function scrollBottom() {
+    //$('textarea').scrollTop($('textarea')[1].scrollHeight);
+    $('textarea').each(function () {
+        $(this).scrollTop($(this).get(0).scrollHeight);
+        //console.log($(this).attr('id'));
+        //console.log($(this).get(0).scrollHeight);
+    })
+}
+function updateUgccMenuItem() {
+    var a = localStorage.getItem('cmd');
+    var b = localStorage.getItem('restart');
+    var c = localStorage.getItem('note');
+    if (a == null)
+        a = false;
+    if (b == null)
+        b = false;
+    if (c == null)
+        c = false;
+    if ($('#restartSpan').length > 0) {
+        if (b == 'true') {
+            //$('#restartpanel').attr('aria-expanded') = 'false';
+            $('#collapseOne').collapse('hide')
+            $('#restartSpan')[0].className = 'glyphicon glyphicon-menu-down';
+        } else {
+            $('#collapseOne').collapse('show');
+            $('#restartSpan')[0].className = 'glyphicon glyphicon-menu-up';
+        }
+    }
+    if ($('#cmdSpan').length > 0) {
+        if (a == 'true') {
+            //$('#restartpanel').attr('aria-expanded') = 'false';
+            $('#collapseCmd').collapse('hide')
+            $('#cmdSpan')[0].className = 'glyphicon glyphicon-menu-down';
+        } else {
+            $('#collapseCmd').collapse('show');
+            $('#cmdSpan')[0].className = 'glyphicon glyphicon-menu-up';
+        }
+    }
+    if ($('#noteSpan').length > 0) {
+        if (c == 'true') {
+            //$('#restartpanel').attr('aria-expanded') = 'false';
+            $('#collapseNote').collapse('hide')
+            $('#noteSpan')[0].className = 'glyphicon glyphicon-menu-down';
+        } else {
+            $('#collapseNote').collapse('show');
+            $('#noteSpan')[0].className = 'glyphicon glyphicon-menu-up';
+        }
+    }
+}
